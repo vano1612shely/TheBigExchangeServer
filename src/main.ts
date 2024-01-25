5c9abefb0ea1cb8963da78c6f94ad31e8707b863
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-01-25 19:15:51 +0200

    add cert

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as bodyParser from "body-parser";
+import { readFileSync } from "fs";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync(
+      "/etc/letsencrypt/live/thebigexchange.net-0001/privkey.pem",
+    ),
+    cert: readFileSync(
+      "/etc/letsencrypt/live/thebigexchange.net-0001/fullchain.pem",
+    ),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.enableCors();
   app.use(bodyParser.json({ limit: "500mb" }));
   app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
