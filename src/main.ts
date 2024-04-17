df9729dc2012ef40b0603ffaecef6302897478ea
Ivan Shelengivskiy <vano1612shely@gmail.com>
2024-04-17 23:30:11 +0300

    123

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as bodyParser from "body-parser";
 import { readFileSync } from "fs";
+import { createServer as createHttpServer } from "http";
+import { createServer as createHttpsServer } from "https";
+import * as express from "express";
+import { ExpressAdapter } from "@nestjs/platform-express";
 async function bootstrap() {
   const httpsOptions = {
     key: readFileSync(
@@ -11,12 +15,13 @@ async function bootstrap() {
       "/etc/letsencrypt/live/thebigexchange.net-0001/fullchain.pem",
     ),
   };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
   app.use(bodyParser.json({ limit: "500mb" }));
   app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
-  await app.listen(5000);
+  createHttpServer(server).listen(5001);
+  createHttpsServer(httpsOptions, server).listen(5000);
+  await app.init();
 }
 bootstrap();
